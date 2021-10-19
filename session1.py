fa977a9d5884a5f7f3a2f7d3d03642d9331a72b7
ryszardnikiel <92529038+ryszardnikiel@users.noreply.github.com>
2021-10-19 14:31:50 +0200

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,29 @@
+# 1- Gerund Slicing
+# Make a function gerund_infinitive that, given a string ending in "ing",
+# returns the rest of the string prefixed with "to ". If the string
+# doesn't end in "ing", return "That's not a gerund!"
+#
+# >>>> gerund_infinitive("building")
+# to build
+# >>>> gerund_infinitive("build")
+# That's not a gerund!
+#######################################################################
+# 2- Oxford Comma
+# Make a function commafy that, given a list of three or more things,
+# returns a list with commas.
+#
+# >>>> commafy(["trinket", "learning", "fun"])
+# trinket, learning, and fun
+# >>>> commafy(["lions", "tigers", "bears"])
+# lions, tigers, and bears
+#######################################################################
+# 3- Switch Case
+# Make a function switch_case that, given a string,
+# returns the string with uppercase letters in lowercase
+# and vice-versa. Include punction and other non-cased
+# characters unchanged.
+#
+# >>>> switch_case("Arg!")
+# aRG!
+# >>>> switch_case("TrInKeT")
+# tRiNkEt
