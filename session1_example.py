d964429d60ff5547af4fc964cc0a2421a1f6802c
ryszardnikiel <92529038+ryszardnikiel@users.noreply.github.com>
2021-10-19 17:27:03 +0200

    Add files via upload

---
+++ session1_example.py
@@ -0,0 +1,57 @@
+# print("hello!")
+#
+# name = "Roman"
+# name = name + ' Nowak'
+# print("hello " + name)
+#
+# number = 11
+#
+# new_number = number + 10
+# print(new_number)  # add
+# print(number + 10)  # add
+#
+# print(number - 5)  #
+# print(number * 5)  # multiply
+# print(number / 2)
+#
+# print(str(number) + name)
+#
+# number = 0
+# if number <= 0:
+#     print('its less than 0')
+# elif number == 0:
+#     print('equal to 0')
+# else:
+#     print('its greater than 0')
+#
+# # payment method challenge
+# # (<10 'pay by cash', >=10 'pay by credit card' <=20, >20 'pay by check'
+#
+# # money = int(input('provide number:\n'))
+# money = 0
+# if money < 10:
+#     print('pay by cash')
+# elif money <= 20:
+#     print('pay by credit card')
+# else:
+#     print('pay by check')
+#
+# print(5 % 2)
+# print(15 % 4)
+#
+# # 2. Challenge
+# # check if the number is odd or even
+# number = int(input('give me the number:\n'))
+# if number % 2 == 0:
+#     print('even')
+# else:
+#     print('odd')
+
+fruits = ['orange', 'bananas', 'lemon']
+print(fruits)
+fruits.append('apple')
+print(fruits)
+fruits.insert(0, 'strawbery')
+print(fruits)
+print(fruits.index('orange'))
+print('length of list:', len(fruits))
